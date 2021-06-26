52ed8a0b96f2ff41d847e17941fefeaa9c6dcbda
AntonioPVI <antoniogarciamontes01@gmail.com>
2021-06-26 15:41:33 +0200

    function randomSquare

--- js/app.js
+++ js/app.js
@@ -5,3 +5,23 @@ let score = document.querySelector('#score');
 
 let result = 0;
 
+function randomSquare() {
+    square.forEach(element => {
+        element.classList.remove('mole');
+    });
+
+    let randomPosition = square[Math.floor(Math.random()*9)];
+    randomPosition.classList.add('mole');
+
+    //asign id of the randomPosition to hitPosition for us to use later
+    hitPosition = randomPosition.id;
+
+    square.forEach(element => {
+        element.addEventListener('mouseup', () => {
+            if (element.id === hitPosition) {
+                result += result;
+                score.textContent = result;
+            }
+        });
+    });
+}
