1286e284854d1ac8578ae7341f6206d481fac37a
AntonioPVI <antoniogarciamontes01@gmail.com>
2021-06-26 15:41:12 +0200

    somes variables

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const square = document.querySelector('.square');
+const mole = document.querySelector('.mole');
+const timeLeft = document.querySelector('#time-left');
+let score = document.querySelector('#score');
+
+let result = 0;
+
